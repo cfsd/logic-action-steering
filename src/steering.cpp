8dd1e17a66fca09e1eebc1ebc288cc4856fd52dd
oscartis <oscar.tisell@gmail.com>
2018-06-24 18:44:45 +0200

    Added ready signal

--- src/steering.cpp
+++ src/steering.cpp
@@ -64,7 +64,14 @@ int32_t main(int32_t argc, char **argv) {
       using namespace std::literals::chrono_literals;
       while (od4.isRunning()) {
         std::this_thread::sleep_for(1s);
+        opendlv::system::SignalStatusMessage heartBeat;
+        heartBeat.code(1);
+
+        std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+        cluon::data::TimeStamp sampleTime = cluon::time::convert(tp);
+
+        od4.send(heartBeat,sampleTime,314);
      }
-    
+
     return retCode;
 }
